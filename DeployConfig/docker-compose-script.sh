25105faccb02bc5d308e6681fcc8ea493bfb2c5f
menghieng-sorn <menghiengsorn.it@gmail.com>
2026-06-20 06:52:51 +0700

    Drop DB build stage; pull mysql image directly from Docker Hub
    - jenkinsfile: remove BUILD DB stage, DB_IMAGE_NAME, and DB_IMAGE arg from deploy
    - DeployConfig/docker-compose.yml: hardcode devopstrainer/java-mvn-privaterepos:mysql; add version
    - DeployConfig/docker-compose-script.sh: remove DB_IMAGE arg, clean up pasted docker-script content
    - db/dockerfile: uncomment MYSQL_ROOT_PASSWORD for parity with pasted version

--- DeployConfig/docker-compose-script.sh
+++ DeployConfig/docker-compose-script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo DOCKER_IMAGE=$1 DB_IMAGE=$2 docker-compose -f /home/ec2-user/DeployConfig/docker-compose.yml up -d
+sudo DOCKER_IMAGE=$1 docker-compose -f /home/ec2-user/DeployConfig/docker-compose.yml up -d
